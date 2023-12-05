fdc2e06d1a3da03bd331e1d00411fdd9e77971e9
claudiasaldivar <claudiasaldivar493@gmail.com>
2023-12-05 10:54:09 -0600

    delete unless elements

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,21 +1,26 @@
 export const getWeather = async (dataCity, setWeather, setError, setLoading, setCity) => {
-  setLoading(true)
-    try {
-      const response = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${dataCity}&limit=1&appid=9a2365c49c12c733f9ca31d301bf6e4c`
-      );
-      const data = response.data[0];
-      setCity(data.name);
-      const weatherResponse = await axios.get(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=hourly,minutely&appid=9a2365c49c12c733f9ca31d301bf6e4c`
-      );
-      setWeather(weatherResponse.data);
-      setError(false);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error al obtener datos del clima:', error);
-      setError(true);
-      setLoading(false);
-      setCity('');
-    }
-  };
+  setLoading(true);
+
+  try {
+    // Obtener datos de ubicación
+    const locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${dataCity}&limit=1&appid=9a2365c49c12c733f9ca31d301bf6e4c`);
+    const locationData = await locationResponse.json();
+    const location = locationData[0];
+
+    setCity(location.name);
+
+    // Obtener datos meteorológicos
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&exclude=hourly,minutely&appid=9a2365c49c12c733f9ca31d301bf6e4c`);
+    const weatherData = await weatherResponse.json();
+
+    setWeather(weatherData);
+    setError(false);
+    setLoading(false);
+  } catch (error) {
+    console.error('Error al obtener datos del clima:', error);
+    setError(true);
+    setLoading(false);
+    setCity('');
+  }
+};
+
